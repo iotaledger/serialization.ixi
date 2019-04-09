3cae57fbd18a2e709a2eba199416a1d32796c5c4
ben-75 <krok.the.fat@gmail.com>
2019-04-09 12:32:35 +0200

    clean up

--- src/main/java/org/iota/ict/ixi/serialization/Main.java
+++ src/main/java/org/iota/ict/ixi/serialization/Main.java
@@ -3,8 +3,6 @@ package org.iota.ict.ixi.serialization;
 import org.iota.ict.Ict;
 import org.iota.ict.utils.properties.Properties;
 
-import java.util.Scanner;
-
 public class Main {
 
 
@@ -14,17 +12,5 @@ public class Main {
         Ict ict = new Ict(properties.toFinal());
         ict.getModuleHolder().loadVirtualModule(SerializationModule.class, "Serialization.ixi");
         ict.getModuleHolder().startAllModules();
-        SerializationModule serializationModule = (SerializationModule) ict.getModuleHolder().getModule("virtual/Serialization.ixi");
-        Scanner scanner = new Scanner(System.in);
-        String input;
-        System.out.print("ict > ");
-        while(!(input = scanner.next()).equalsIgnoreCase("q")){
-
-            if("q".equalsIgnoreCase(input)){
-                break;
-            }
-            System.out.println(input);
-            System.out.println("ict > ");
-        }
     }
 }
