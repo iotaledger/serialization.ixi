344e7d1f751a344f8cf6164f31ff3ea0f2d5989e
ben-75 <krok.the.fat@gmail.com>
2019-04-18 18:58:56 +0200

    remove dead code

--- src/main/java/org/iota/ict/ixi/serialization/util/SerializableField.java
+++ src/main/java/org/iota/ict/ixi/serialization/util/SerializableField.java
@@ -10,6 +10,5 @@ import java.lang.annotation.Target;
 public @interface SerializableField {
     int index();
     long tritLength();
-    String label() default "";
     Class<? extends TritsConverter> converter();
 }
--- src/test/java/org/iota/ict/ixi/serialization/model/SampleSerializableClass.java
+++ src/test/java/org/iota/ict/ixi/serialization/model/SampleSerializableClass.java
@@ -7,18 +7,18 @@ import java.util.List;
 
 public class SampleSerializableClass {
 
-    @SerializableField(index = 0, tritLength = 1, label = "isTest", converter = TritsConverter.BooleanConverter.class)
+    @SerializableField(index = 0, tritLength = 1, converter = TritsConverter.BooleanConverter.class)
     public boolean isTest;
 
-    @SerializableField(index = 1, tritLength = 99, label = "myLabel", converter = TritsConverter.AsciiConverter.class)
+    @SerializableField(index = 1, tritLength = 99, converter = TritsConverter.AsciiConverter.class)
     public String myLabel;
 
-    @SerializableField(index = 2, tritLength = 243, label = "aReference", converter = TritsConverter.TrytesConverter.class)
+    @SerializableField(index = 2, tritLength = 243, converter = TritsConverter.TrytesConverter.class)
     public String aReferenceHash;
 
-    @SerializableField(index = 3, tritLength = 243, label = "aReferenceList", converter = TritsConverter.TrytesConverter.class)
+    @SerializableField(index = 3, tritLength = 243, converter = TritsConverter.TrytesConverter.class)
     public List<String> listOfReferences;
 
-    @SerializableField(index = 4, tritLength = 27, label = "myInteger", converter = TritsConverter.IntegerConverter.class)
+    @SerializableField(index = 4, tritLength = 27, converter = TritsConverter.IntegerConverter.class)
     public int myInteger;
 }
